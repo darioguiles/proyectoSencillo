ee020d2084460d35bf94381425b8ce8c31e2b6a2
darioguiles <dario.guilescuesta@hotmail.com>
2023-10-17 12:16:25 +0200

    2.0

--- pruebaJenkins/src/main/java/Main.java
+++ pruebaJenkins/src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        //Este proyecto está bien
+        System.out.println("Adios Mundo");
     }
 }
